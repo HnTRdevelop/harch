5cd2e1b0dd4272cda108029789147ff764e86f95
hntrdevelop <sergeisiviz@gmail.com>
2025-02-10 20:53:04 +0500

    init

---
+++ files/.config/waybar/getplayed.py
@@ -0,0 +1,21 @@
+from os import popen
+
+
+status = popen("playerctl status").read().strip().lower()
+if status != "":
+    metadata = dict()
+    for i in popen("playerctl metadata").read().split("\n"):
+        if i == "":
+            continue
+        p = i.find(" ", i.find(":"))
+        param = i[i.find(":") + 1:p]
+        while i[p] == " ":
+            if p == len(i) - 1:
+                break
+            p += 1
+        value = i[p:]
+        metadata[param] = value
+
+    print(f"{"" if status == "playing" else ""} {metadata["artist"]}: {metadata["album"]} - {metadata["title"]}".replace("&", "and"))
+else:
+    print("No player")
---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo HArch - HnTR arch + hyprland installation script
+echo This script will install and configure everything automatically
+
+while :;
+do
+    echo "Proceed? [y/N]"
+    read
+    echo 
+    if [ "$REPLY" = "n" ] || [ "$REPLY" = "N" ] || [ "$REPLY" = "" ]; then
+        echo canceling...
+        exit
+    elif [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ]; then
+        echo proceeding...
+        break
+    fi
+done
+
+
+if [ "$(pacman -Qsq yay)" != "yay" ]; then
+    echo firstly install yay
+    exit
+fi
+
+echo
+echo installing needed packages
+
+doas yay -Syu hyprland waybar hyprshot hyprpaper hyprlock hyprpicker copyq nekoray firefox nemo dunst gedit kitty wofi gnome-calculator vscodium pwvucontrol ponymix brightnessctl hyprpicker gnome-calendar loupe blueman-manager adwaita-qt6-git adwaita-qt5-git viu
+
+echo 
+echo clearing .config folder...
+here=$(pwd)
+to_remove=$(ls "$(pwd)/files/.config")
+cd ~/.config
+rm -r $to_remove
+cd $here
+
+echo
+echo copying configuration files...
+cp -r ./files/.config/* ~/.config/
+cp -r ./files/.icons/ ~/
+cp -r ./files/.themes ~/
+
+echo
+echo
+echo All done!
